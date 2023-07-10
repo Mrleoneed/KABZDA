993bac7041d902b3e0a91d53a1babedadc2790f3
Mrleoneed <mrleoneed@gmail.com>
2023-07-10 11:57:10 +0300

    Less 06: useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.css';
 import Accordion from "./Components/Accordion/Accordion";
 import {Rating} from "./Components/Rating/Rating";
+import {OnOff} from "./Components/OnOff/OnOff";
+import UnControlledAccordion from "./Components/UnControlledAccordion/UnControlledAccordion";
 
 
 function App() {
     console.log('App Rendered')
     return (
         <div>
-            <Accordion titleValue={"Меню"} collapsed={true}/>
-            <Accordion titleValue={"Пользователи"} collapsed={false}/>
+
+            <OnOff/>
+            <OnOff/>
+
+            <UnControlledAccordion titleValue={"Меню"}/>
+            <UnControlledAccordion titleValue={"Меню"} />
+
             <Rating value={5}/>
         </div>
     );
---
+++ src/Components/UnControlledAccordion/UnControlledAccordion.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+
+function UnControlledAccordion(props: AccordionPropsType) {
+let [collapsed,setCollapsed]= useState(true)
+
+
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            <button>Toggle</button>
+            {!collapsed && <AccordionBody/>}
+        </div>
+    )
+}
+
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+
+    return (
+        <>
+            <h3>{props.title}</h3>
+        </>
+    )
+}
+
+function AccordionBody() {
+    console.log('AccordionBody Rendered')
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    )
+}
+
+export default UnControlledAccordion
